95d55bd785f5685dba9f809ffb8533d79eb3b9fd
Karan Pahlani <pahlani@gmail.com>
2020-06-12 15:40:50 -0500

    CRUD operations framework for EmployeeController

--- src/main/java/com/fleet/controller/EmployeeController.java
+++ src/main/java/com/fleet/controller/EmployeeController.java
@@ -2,29 +2,39 @@ package com.fleet.controller;
 
 import com.fleet.entity.Employee;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Component
+//@Controller // similar to @Component
+//@ResponseBody
+@RestController
+@RequestMapping(value = "employees")
 public class EmployeeController {
 
+    @RequestMapping(method = RequestMethod.GET)
     public List<Employee> findAll(){
         return null;
     }
 
-    public Employee findOne(String id){
+    @RequestMapping(method = RequestMethod.GET, value = "{id}")
+    public Employee findOne(@PathVariable("id") String empid){
         return null;
     }
 
-    public Employee create(Employee employee){
+    @RequestMapping(method = RequestMethod.POST)
+    public Employee create(@RequestBody Employee employee){
         return employee;
     }
 
-    public Employee update(String id, Employee employee){
+    @RequestMapping(method = RequestMethod.PUT, value = "{id}")
+    public Employee update(@PathVariable("id") String empid, @RequestBody Employee employee){
         return employee;
     }
 
-    public void delete(String id){
+    @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+    public void delete(@PathVariable("id") String empid){
 
     }
 }
